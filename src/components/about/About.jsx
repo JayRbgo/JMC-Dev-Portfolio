b4294d2479157ffebe7b5f60168ff08cf7bc2b8b
Mounirlak75 <lakehal.mounir3@gmail.com>
2022-09-25 03:30:08 +0200

    diff

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./about.css"
+import ME from '../../assets/me.jpg'
+import {CgAwards} from 'react-icons'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about">
+      <h5>Faire connaissance</h5>
+      <h2>A propos de nous !</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+            <CgAwards className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1 an d'experice !</small>
+            </article>
+
+            <article className='about__card'>
+            <CgAwards className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1 an d'experice !</small>
+            </article>
+
+            <article className='about__card'>
+            <CgAwards className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1 an d'experice !</small>
+            </article>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
