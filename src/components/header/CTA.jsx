76aeef9f63256956550c8e44b2283715e06ce1ff
jayrbgo <jay.rbgo@gmail.com>
2022-09-25 04:39:01 +0200

    Debut du Header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href="#">Brochure PDF</a>
+      <a href="#contact">Nous Contacter</a>
+    </div>
+  );
+};
+
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import "./header.css"
-
+import "./header.css";
+import CTA from "./CTA";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="container header-container">
+        <h5>Bonjour nous sommes là</h5>
+        <h1>JMC Team Dev</h1>
+        <h5>Développement Web Pro</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
